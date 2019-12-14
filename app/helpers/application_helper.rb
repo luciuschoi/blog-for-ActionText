af15eda0f4b6434193c74f7f448f0ab9c5d0b849
Lucius Choi <lucius.choi@gmail.com>
2019-12-14 19:55:40 +0900

    fa_icon() 헬퍼 메소드 작성하기

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  STYLES = {
+    regular: 'r',
+    solid: 's',
+    brands: 'b',
+    light: 'l',
+    duotone: 'd'
+  }.freeze
+
+  def fa_icon(style, fontname)
+    content_tag(:i, '', class: "fa#{STYLES[style]} fa-#{fontname}")
+  end 
 end
